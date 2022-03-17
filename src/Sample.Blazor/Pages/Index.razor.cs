b766ff2cb8ad2f76194e84c10b0c0aa35421e50f
GAPAGRO <GAPAGRO@YAZILIM-MDY>
2022-03-17 10:02:28 +0300

    Add project files.

---
+++ src/Sample.Blazor/Pages/Index.razor.cs
@@ -0,0 +1,6 @@
+﻿namespace Sample.Blazor.Pages;
+
+public partial class Index
+{
+
+}
---
+++ src/Sample.Blazor/SampleComponentBase.cs
@@ -0,0 +1,12 @@
+﻿using Sample.Localization;
+using Volo.Abp.AspNetCore.Components;
+
+namespace Sample.Blazor;
+
+public abstract class SampleComponentBase : AbpComponentBase
+{
+    protected SampleComponentBase()
+    {
+        LocalizationResource = typeof(SampleResource);
+    }
+}
